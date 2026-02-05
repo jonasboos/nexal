fa2baa2823018a9b5d375942a131a103ecb450a5
Jonas Boos <jonas.boos@bluewin.ch>
2026-02-05 23:08:51 +0100

    update

--- templates/v2/src/components/nav-bar.tsx
+++ templates/v2/src/components/nav-bar.tsx
@@ -37,7 +37,7 @@ export function NavBar() {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-  // Scroll to top on route changes
+  // Scroll to top on route change
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
